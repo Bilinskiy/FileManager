4e1f2f1594b538c534518448100c9b9fab4667a2
Bilinskiy <bilinskijwork@gmail.com>
2024-10-25 11:55:54 +0300

    add NavigationController / UITableView / ButtonPlusFolder

--- FileManager/SceneDelegate.swift
+++ FileManager/SceneDelegate.swift
@@ -16,12 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    guard let windowScene = (scene as? UIWindowScene) else { return }
-            
-            window = UIWindow(windowScene: windowScene)
-            window?.windowScene = windowScene
+    guard let scene = (scene as? UIWindowScene) else { return }
+          
+    let navBar = UINavigationController(rootViewController: ViewController())
+
+            window = UIWindow(windowScene: scene)
+            window?.windowScene = scene
             window?.makeKeyAndVisible()
-            window?.rootViewController = ViewController()
+            window?.rootViewController = navBar
+    
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
--- FileManager/ViewController.swift
+++ FileManager/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+  lazy var tableView: UITableView = {
+    var table = UITableView()
+    table.rowHeight = 40
+    table.layer.cornerRadius = 15
+    table.dataSource = self
+    table.delegate = self
+    return table
+  }()
+  
+  lazy var rightBarButtonPlusFolder: UIBarButtonItem = {
+    var button = UIBarButtonItem(image: UIImage(systemName: "folder.badge.plus"), style: .plain, target: self, action: #selector(plusFolder))
+    return button
+  }()
+  
+
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+ 
+    view.backgroundColor = .white
+    settingsNavigationController()
+    
+    view.addSubview(tableView)
+    
+    updateViewConstraints()
+  }
+  
+  @objc func plusFolder() {
+    print ("plus folder")
+  }
+  
+  func settingsNavigationController () {
+    navigationItem.title = "Просмотр каталога"
+    navigationItem.rightBarButtonItems = [rightBarButtonPlusFolder]
+    navigationController?.navigationBar.tintColor = .black
+    
+    navigationController?.navigationBar.scrollEdgeAppearance = UINavigationBarAppearance()
+    navigationController?.navigationBar.scrollEdgeAppearance?.backgroundColor = .orange
+   
+  }
+  
+  override func updateViewConstraints() {
+    super.updateViewConstraints()
+
+    tableView.snp.makeConstraints { make in
+      make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(16)
+      make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+      make.leading.equalToSuperview().inset(16)
+      make.trailing.equalToSuperview().inset(16)
+
+    }
+    
   }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    8
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+  
+  
+  
+  
+  
+  
 }
 
